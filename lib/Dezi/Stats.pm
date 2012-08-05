155d9f06ef8238cff676d6775a84e6ba894da1b6
Peter Karman <karpet@peknet.com>
2012-08-04 23:49:24 -0500

    init

---
+++ lib/Dezi/Stats.pm
@@ -0,0 +1,72 @@
+package Dezi::Stats;
+
+use warnings;
+use strict;
+
+our $VERSION = '0.001000';
+
+=head1 NAME
+
+Dezi::Stats - log statistics for your Dezi server
+
+=head1 SYNOPSIS
+
+ % dezi --stats=sqlite
+
+=cut
+
+1;
+
+__END__
+
+=head1 AUTHOR
+
+Peter Karman, C<< <karman at cpan.org> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-dezi-stats at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Dezi-Stats>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Dezi::Stats
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Dezi-Stats>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Dezi-Stats>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Dezi-Stats>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Dezi-Stats/>
+
+=back
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2012 Peter Karman.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+=cut
+
